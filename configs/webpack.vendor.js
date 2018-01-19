69eef8ab09dfcdd3088b146d868b7c549223961c
anonymous0374 <377738226@qq.com>
2018-01-19 14:31:50 +0800

    update vendor related config

--- configs/webpack.vendor.js
+++ configs/webpack.vendor.js
@@ -3,17 +3,16 @@ var path = require('path')
 
 module.exports = {
   entry: {
-    vendor: [path.resolve(__dirname, '../src/vendor/dev/jquery.js'),
-      path.resolve(__dirname, '../src/vendor/dev/bootstrap/dist/js/bootstrap.js')
-    ]
+    jquery: ['jquery'],
+    bootstrap: ['bootstrap/dist/js/bootstrap.js']
   },
   output: {
-    filename: 'vendor.bundle.js',
-    path: path.resolve(__dirname, '../dist/vendor'),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../vendor'),
     library: 'vendor_lib'
   },
   plugins: [new webpack.DllPlugin({
     name: 'vendor_lib',
-    path: './dist/vendor/vendor-manifest.json'
+    path: path.resolve(__dirname, '../vendor/vendor-manifest.json')
   })]
 }
