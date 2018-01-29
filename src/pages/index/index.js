8edec7e2657c6c3e38a2bc47a5cfa5aac8b62538
anonymous0374 <377738226@qq.com>
2018-01-29 16:24:55 +0800

    add title and description to sliders

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -20,19 +20,19 @@ $((function () {
   let sliders = [{
     url: '../styles/images/sliders/slider-i-01.jpg',
     alt: '武汉麦念科技信息有限公司',
-    title: '',
-    description: '',
+    title: '专家团队筹划',
+    description: '专家团队筹划，制定系统方案，无死角精准节税',
     active: true
   }, {
     url: '../styles/images/sliders/slider-i-03.jpg',
     alt: '武汉麦念科技信息有限公司',
-    title: '',
-    description: ''
+    title: '创先进企业',
+    description: '创先进企业，享百万补贴，助企业腾飞'
   }, {
     url: '../styles/images/sliders/slider-i-04.jpg',
     alt: '武汉麦念科技信息有限公司',
-    title: '',
-    description: ''
+    title: '一站式服务',
+    description: '注册，代帐，网站，一站式服务'
   }]
   let toper = {
     message: '您好！欢迎访问武汉麦念科技有限公司官网！',
